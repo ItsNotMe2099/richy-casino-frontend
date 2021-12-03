f096a7bd47b5586f626d6f03ae5503ddf334b7b5
Konstantin Ostroukhov <ostrouhovk@yandex.ru>
2021-12-03 20:50:35 +0700

    add grid system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,16 @@ import 'normalize.css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { setConfiguration } from 'react-grid-system'
 import { AppWrapper } from 'context/state'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  setConfiguration({
+    gutterWidth: 20,
+    breakpoints: [700, 980, 1360],
+    containerWidths: [680, 960, 1320],
+  })
+
   return (
     <AppWrapper>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { GetStaticProps, GetStaticPropsContext } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import styles from 'pages/index.module.scss'
+import { Container } from 'react-grid-system'
 
 export default function IndexPage() {
   return (
     <div className={styles.root}>
-      HomePage
+      <Container>HomePage</Container>
     </div>
   )
 }
