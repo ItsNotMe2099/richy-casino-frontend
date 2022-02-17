ec8cce66ce45a89469dd5343d565c4f82bfa76e2
Andrey <a1081515@yandex.ru>
2022-02-17 14:38:27 +0300

    fix header

--- components/layout/Layout/index.tsx
+++ components/layout/Layout/index.tsx
@@ -3,22 +3,30 @@ import Footer from '../Footer'
 import Header from '../Header'
 import styles from './index.module.scss'
 import Menu from 'components/layout/Menu'
+import {StickyContainer} from 'react-sticky'
+import {useAppContext} from 'context/state'
 
 interface Props{
   children?: React.ReactNode
 }
 
 export default function Layout({children}: Props){
+  const appContext = useAppContext()
+  const renderMainContent = () => {
+    return <>
+      <Menu/>
 
+      {children}
+      <Footer/>
+      </>
+  }
    return (
     <Container fluid style={{height: '100%', minHeight: '100%', maxWidth: '1320px'}}>
     <div className={styles.root}>
-
         <Header/>
-
-      <Menu/>
-        {children}
-      <Footer/>
+      {appContext.isDesktop ? <StickyContainer>
+        {renderMainContent()}
+      </StickyContainer> : renderMainContent()}
     </div>
     </Container>
   )
