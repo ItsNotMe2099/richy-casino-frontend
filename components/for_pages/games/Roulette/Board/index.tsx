8487b5541c307ad7e07be95d8d7e54c8a74967f1
Andrey <a1081515@yandex.ru>
2022-04-01 18:22:07 +0300

    improve roulette

--- components/for_pages/games/Roulette/Board/index.tsx
+++ components/for_pages/games/Roulette/Board/index.tsx
@@ -46,8 +46,10 @@ export default function Board(props: Props) {
   return (
     <GamePageBoardLayout>
       <div className={styles.root}>
+        <div className={styles.wheel}>
         <WheelCanvas width={'600px'} height={'600px'} number={result?.data.slot} hasResult={!!result}/>
-        <div className={styles.totalBet}>Общая ставка <span className={styles.bet}>{Object.keys(props.bets).map(i => props.bets[i]).reduce((a, b) => a + b, 0).toFixed(8)}</span> {gameContext.user?.currency ?? ''}</div>
+        </div>
+        <div className={styles.totalBet}>Общая ставка <span className={styles.bet}>{Object.keys(props.bets).map(i => props.bets[i]).reduce((a, b) => a + b, 0).toFixed(8)}</span> {gameContext.user?.currency?.toUpperCase() ?? ''}</div>
         <BetBoard chip={props.chip} bets={props.bets} onBet={props.onBet}/>
         <Toolbar onUndo={props.onUndo} onClear={props.onClear} bets={props.bets}/>
       </div>
