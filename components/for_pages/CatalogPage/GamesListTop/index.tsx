b8c5b71a48102fda5ede799935580090925b8c12
Nikita <wrongnmail@yandex.ru>
2022-04-07 12:42:59 +0300

    pixel perfect desktop 1

--- components/for_pages/CatalogPage/GamesListTop/index.tsx
+++ components/for_pages/CatalogPage/GamesListTop/index.tsx
@@ -4,6 +4,7 @@ import SwitchFilter from 'components/for_pages/Common/SwitchFilter'
 import Slider from 'react-slick'
 import HiddenXs from 'components/ui/HiddenXS'
 import VisibleXs from 'components/ui/VisibleXS'
+import { useState } from 'react'
 
 interface IGame{
   label: string
@@ -17,9 +18,14 @@ interface IGame{
 
 interface Props {
   items?: IGame[]
+  slider?: any
 }
 
 export default function GamesListTop(props: Props) {
+
+  let { slider } = props
+
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const Item = (prop:{item: IGame}) => {
 
@@ -41,38 +47,32 @@ export default function GamesListTop(props: Props) {
 
   const settings = {
     className: `${styles.list}`,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 1,
-    variableWidth: false,
-    adaptiveHeight: false,
+    variableWidth: true,
+    adaptiveHeight: true,
     arrows: false,
     dotsClass: `${styles.dots}`,
+    beforeChange: (current: number, next: number) => setCurrentIndex(next),
 
-    responsive: [
-      {
-        breakpoint: 1495,
-        settings: {
-          slidesToShow: 5,
-        }
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 4,
-        }
-      },
-    ]
   }
 
   return (
     <div className={styles.root}>
-      <Header icon='/img/Contents/money.svg' label='ТОП игры' length={1} shadowColor='red' style='fullOnlyOnMobile'/>
+      <Header
+        icon='/img/Contents/money.svg'
+        label='ТОП игры'
+        shadowColor='red'
+        style='fullOnlyOnMobile'
+        onPrev={() => slider.slickGoTo(currentIndex - 1)}
+        onNext={() => slider.slickGoTo(currentIndex + 1)}
+        slider />
       <div className={styles.wrapper}><SwitchFilter top/></div>
       <HiddenXs>
-        <Slider {...settings}>
-          {props.items && props.items.slice(0, 6).map((item, index) =>
+        <Slider {...settings} ref={slider1 => (slider = slider1)}>
+          {props.items && props.items.map((item, index) =>
             <Item item={item} key={index}/>
           )}
         </Slider>
