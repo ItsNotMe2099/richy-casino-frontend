7694aa7f3eb84d2228543b4ce80e30a1a0f0fc17
Konstantin Ostroukhov <ostrouhovk@yandex.ru>
2022-04-11 20:47:49 +0700

    limbo: events

--- components/for_pages/games/Limbo/Board/index.tsx
+++ components/for_pages/games/Limbo/Board/index.tsx
@@ -1,25 +1,47 @@
 import styles from './index.module.scss'
 import GamePageBoardLayout from 'components/for_pages/games/components/layout/GamePageBoardLayout'
-import {useGameContext} from 'components/for_pages/games/context/state'
+import { useGameContext } from 'components/for_pages/games/context/state'
 import { useEffect, useRef, useState } from 'react'
-import {ICasinoGameFinishEvent} from 'components/for_pages/games/data/interfaces/ICasinoGame'
+import { ICasinoGameFinishEvent } from 'components/for_pages/games/data/interfaces/ICasinoGame'
 import { StateMachineInput } from 'rive-react'
 import RiveStateMachine from 'components/ui/RiveStateMachine'
+import { GameStage } from './constants'
 
 interface Props{}
+
+interface EventData {
+  target: number
+  number: number
+}
 
 export default function Board(props: Props) {
   const gameContext = useGameContext()
   const [result, setResult] = useState<ICasinoGameFinishEvent>(null)
   const inputRocketRef = useRef<StateMachineInput>(null)
   const inputPlanetsRef = useRef<StateMachineInput>(null)
+  const [gameStage, setGameStage] = useState(GameStage.idle)
+  const gameStageRef = useRef<GameStage>(gameStage)
 
   useEffect(() => {
-    const subscription = gameContext.gameState$.subscribe((data) => {
-      if (data && inputRocketRef.current) {
-        setResult(data)
-        inputRocketRef.current.fire()
+    gameStageRef.current = gameStage
+  }, [gameStage])
+
+  useEffect(() => {
+    const subscription = gameContext.gameState$.subscribe((e) => {
+      if (e && inputRocketRef.current && gameStageRef.current === GameStage.idle) {
+        const data = e.data as EventData
+        console.log('log: ', data)
+        setResult(e)
+        setGameStage(GameStage.inProgress)
         inputPlanetsRef.current.fire()
+        setTimeout(() => {
+          inputRocketRef.current.fire()
+          setTimeout(() => {
+            setGameStage(GameStage.idle)
+            inputPlanetsRef.current.fire()
+            inputRocketRef.current.fire()
+          }, 2000)
+        }, 1000)
       }
     })
     return () => {
@@ -30,16 +52,20 @@ export default function Board(props: Props) {
   return (
     <GamePageBoardLayout>
       <div className={styles.root}>
-        <RiveStateMachine
-          src="/animations/limbo/planets.riv"
-          inputRef={inputPlanetsRef}
-          className={styles.planets}
-        />
-        <RiveStateMachine
-          src="/animations/limbo/rocket.riv"
-          inputRef={inputRocketRef}
-          className={styles.rocket}
-        />
+        <div className={styles.planetsLayer}>
+          <RiveStateMachine
+            src="/animations/limbo/planets.riv"
+            inputRef={inputPlanetsRef}
+            className={styles.planets}
+          />
+        </div>
+        <div className={styles.rocketLayer}>
+          <RiveStateMachine
+            src="/animations/limbo/rocket.riv"
+            inputRef={inputRocketRef}
+            className={styles.rocket}
+          />
+        </div>
       </div>
     </GamePageBoardLayout>
   )
