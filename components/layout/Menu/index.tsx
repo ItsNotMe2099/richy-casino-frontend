c819c6ffc507fbea28e405406bea39aea67be4fb
Andrey <a1081515@yandex.ru>
2022-04-25 23:12:47 +0300

    fix links

--- components/layout/Menu/index.tsx
+++ components/layout/Menu/index.tsx
@@ -8,8 +8,8 @@ import classNames from 'classnames'
 import {Sticky} from 'react-sticky'
 import {useAppContext} from 'context/state'
 import {useTranslation} from 'next-i18next'
-import {LINKS} from 'types/constants'
 import {ModalType} from 'types/enums'
+import {Routes} from 'types/routes'
 
 interface Props {
   children?: React.ReactNode
@@ -37,17 +37,17 @@ export default function Menu(props: Props) {
   const {route: currentRoute, asPath: currentPath} = useRouter()
   const options = [
     {label: t('menu_main'), link: '/'},
-    {label: t('menu_casino'), link: '/catalog'},
-    {label: t('menu_richy'), link: LINKS.RICHY_GAMES},
-    {label: t('menu_free_bitcoin'), link: '/freebitcoin'},
+    {label: t('menu_casino'), link: Routes.catalog},
+    {label: t('menu_richy'), link: Routes.richyGames},
+    {label: t('menu_free_bitcoin'), link: Routes.freeBitcoin},
     {label: t('menu_wheel_of_fortune'), link: '/wheel_of_fortune', onClick: () => appContext.showModal(ModalType.fortune)},
-    {label: t('menu_lottery'), link: '/lottery'},
-    {label: t('menu_live_casino'), link: '/catalog/category/live'},
-    {label: t('menu_aviator'), link: '/aviator'},
-    {label: t('menu_poker'), link: '/poker'},
+    {label: t('menu_lottery'), link: Routes.catalogLive},
+    {label: t('menu_live_casino'), link: Routes.catalogLive},
+    {label: t('menu_aviator'), link: Routes.aviator},
+    {label: t('menu_poker'), link: Routes.poker},
     {label: t('menu_tournaments'), link: '/tournaments'},
-    {label: t('menu_bonuses'), link: '/bonuses'},
-    {label: t('menu_chess'), link: '/chess'},
+    {label: t('menu_bonuses'), link: Routes.bonuses},
+    {label: t('menu_chess'), link: Routes.chess},
   ]
   const renderMobile = () => {
     return  <div className={classNames(styles.menu, )} >
