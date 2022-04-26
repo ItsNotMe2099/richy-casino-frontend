a38124f7ed71d690a4d95fa5186d7c8a34c15d03
Andrey <a1081515@yandex.ru>
2022-04-26 13:50:55 +0300

    fix menu link

--- components/layout/Menu/MenuItem/index.tsx
+++ components/layout/Menu/MenuItem/index.tsx
@@ -2,6 +2,8 @@ import styles from './index.module.scss'
 import classNames from 'classnames'
 import Link from 'next/link'
 import useIsActiveLink from 'hooks/useIsActiveLink'
+import {Routes} from 'types/routes'
+import {useRouter} from 'next/router'
 
 interface IOption{
   label: string
@@ -16,7 +18,12 @@ interface Props {
 }
 
 export default function MenuItem(props: Props) {
-  const active = useIsActiveLink(props.link)
+    const { asPath } = useRouter()
+  const key = asPath.replace('/', '')
+  let active = useIsActiveLink(props.link)
+  if( Routes.catalog === props.link && key && [Routes.catalogLive, Routes.richyGames].map(i => i.replace('/', '')).includes(key)){
+    active = false
+  }
   const handleClick = (e) => {
     if(props.onClick){
       e.preventDefault()
