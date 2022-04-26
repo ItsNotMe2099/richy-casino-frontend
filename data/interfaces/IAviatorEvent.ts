4b386c0e3c050390b08fe6df4d2ddd41281cc797
Konstantin Ostroukhov <ostrouhovk@yandex.ru>
2022-04-26 17:04:13 +0700

    CAS-64 add api to aviator

--- data/interfaces/IAviatorEvent.ts
+++ data/interfaces/IAviatorEvent.ts
@@ -1,3 +1,29 @@
-export interface IAviatorEvent{
+export enum AviatorEventType {
+  planned = 'planned',
+  bet = 'bet',
+  started = 'started',
+  multiplierUpdate = 'multiplierUpdate',
+  betFinished = 'betFinished',
+  finished = 'finished',
+}
 
+export enum AviatorRoundStatus {
+  created = 'created',
+  inProgress = 'inProgress',
+  finished = 'finished',
+}
+
+export interface IAviatorRound {
+  id: number
+  plannedStartAfter: number // ms
+  plannedStartAt: string
+  startedDuration: number // ms
+  status: AviatorRoundStatus
+}
+
+export interface IAviatorEvent{
+  accountId: number
+  type: AviatorEventType
+  round: IAviatorRound
+  multiplier: number
 }
