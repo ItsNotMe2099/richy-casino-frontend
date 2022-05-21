0f0e789f4eb0cace4eb16b2081d2ee651bdde006
Nikita <wrongnmail@yandex.ru>
2022-05-21 13:41:38 +0300

    Bonuses page, banners from backend, cosmetic fixes

--- pages/bonuses/index.tsx
+++ pages/bonuses/index.tsx
@@ -7,37 +7,41 @@ import {useTranslation} from 'next-i18next'
 import BonusSlide from 'components/for_pages/Common/BonusSlide'
 import Button from 'components/ui/Button'
 import classNames from 'classnames'
+import { useAppContext } from 'context/state'
+import Image from 'next/image'
+import { useMeasure } from 'react-use'
 
 export default function Bonuses(){
 
   const {t} = useTranslation()
 
-  const items =[
-    {label: <div>Лучшие игры от Richy</div>, image: '/img/TopSlider/banner@3x.png'},
-    {label: <div className={styles.itemLabel}>Spin the <span className={styles.spin}>wheel of fortune</span><br/> every day and get a<br/> guaranteed <span className={styles.spin}>prizes</span></div>, image: '/img/TopSlider/wheel@3x.png'},
-    {label: <div className={styles.itemLabel}>Try your luck in the <span className={styles.lottery}>most profitable</span><br/> cryptocurrency lottery<br/> and get bonuses</div>, image: '/img/TopSlider/lottery@3x.png'},
-  ]
+  const context = useAppContext()
+
+  const isMobile = context.isMobile
+
+  const [ref, { width, height }] = useMeasure()
 
   return (
     <Layout>
       <NextSeo title={t('page_index_title')}/>
-        <div className={styles.root}>
+        <div className={styles.root} ref={ref}>
           <div className={styles.title}>Бонусы</div>
           <div className={styles.grid}>
             <BonusSlide/>
-            {items.map((item, index) =>
-              <div className={styles.rootSlide} key={index}>
-                <div className={styles.item} style={{backgroundImage: `url(${item.image})`}}>
-                  <div className={styles.left}>
-                    <div className={classNames({[styles.label]: index == 0})}>
-                      {item.label}
-                    </div>
-                    <div className={styles.btn}>
-                      <Button size='normal' background='white'>Начать играть</Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            {context.banners.map((item, index) => <div className={styles.rootSlide} key={index}>
+            <div className={styles.item}>
+          {(item.imageDesktopUrl || item.imageMobileUrl) && <Image src={item.imageDesktopUrl || item.imageMobileUrl} layout={'fill'}/>}
+
+          <div className={styles.left}>
+          <div className={classNames(styles.label, {[styles.second]: index > 0})} style={{fontSize: isMobile ? `${width / 24}px` : `${width / 54}px`}}>
+            {item.title}
+          </div>
+          <div className={classNames(styles.btn, {[styles.alt]: index === 1})} style={{fontSize: isMobile ? `${width / 25}px` : `${width / 77}px`}}>
+            <Button size='normal' background={index === 1 ? 'blueGradient500' : 'white'} href={item.redirectUrl}>{item.textButton}</Button>
+          </div>
+        </div>
+        </div>
+        </div>
             )}
           </div>
         </div>
