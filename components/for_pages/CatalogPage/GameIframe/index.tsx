e745057f7e6c39c1a0949a6736f7081044cf3c52
Andrey <a1081515@yandex.ru>
2022-06-16 14:28:23 +0300

    fix game header

--- components/for_pages/CatalogPage/GameIframe/index.tsx
+++ components/for_pages/CatalogPage/GameIframe/index.tsx
@@ -1,7 +1,6 @@
 import styles from './index.module.scss'
 import {IGame} from 'data/interfaces/IGame'
 import {IGameSession} from 'data/interfaces/IGameSession'
-import GameIFrameHeader from 'components/for_pages/CatalogPage/GameIframe/GameIFrameHeader'
 import {useTranslation} from 'next-i18next'
 
 interface Item extends IGame{
@@ -15,7 +14,6 @@ export default function GameIframe(props: Props) {
   const {t} = useTranslation()
   return (
     <div className={styles.root}>
-      <GameIFrameHeader icon={''} title={'Игра'}/>
       <div className={styles.board}>
         <div className={styles.iframeWrapper}>
           {props.session && <iframe
