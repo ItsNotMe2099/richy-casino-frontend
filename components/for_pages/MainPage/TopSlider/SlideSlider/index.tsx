a2f3a44551bc63986d6c73477fc5e8f362a1c35a
Nikita <wrongnmail@yandex.ru>
2022-07-01 09:29:02 +0300

    fix btns on bonuses

--- components/for_pages/MainPage/TopSlider/SlideSlider/index.tsx
+++ components/for_pages/MainPage/TopSlider/SlideSlider/index.tsx
@@ -89,8 +89,8 @@ export default function SlideSlider(props: Props) {
           <div className={classNames(styles.label, {[styles.second]: index > 0})} style={{fontSize: `${width / 24}px`}}>
             {item.title}
           </div>
-          <div className={classNames(styles.btn, {[styles.alt]: item.id === 3})} style={{fontSize: isMobile ? `${width / 22}px` : `${width / 38 > 18 ? 18 : width / 38}px`}}>
-            <Button size='normal' background={index === 1 ? 'blueGradient500' : 'white'} href={item.redirectUrl}>{item.textButton}</Button>
+          <div className={classNames(styles.btn)} style={{fontSize: isMobile ? `${width / 22}px` : `${width / 38 > 18 ? 18 : width / 38}px`}}>
+            <Button size='normal' background={'white'} href={item.redirectUrl}>{item.textButton}</Button>
           </div>
         </div>
         </div>
