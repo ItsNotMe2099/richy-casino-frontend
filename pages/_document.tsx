001f199590e21460e5081a49ad08d2a498145fb0
Andrey <a1081515@yandex.ru>
2022-07-28 14:14:19 +0300

    fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -106,17 +106,7 @@ class MyDocument extends Document {
 
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-               var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/628546d9b0d10b6f3e72e8f1/1g3c9ph38';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-        })();
-            `,
+            __html:'',
           }}
         />
         </body>
