0fbdfd9c32e5711f0595c90b48102bf8cbd6967f
Andrey <a1081515@yandex.ru>
2022-08-17 09:55:55 +0300

    fix iframe resizer

--- components/for_pages/CatalogPage/GameIframeRichy/index.tsx
+++ components/for_pages/CatalogPage/GameIframeRichy/index.tsx
@@ -5,6 +5,8 @@ import {useTranslation} from 'next-i18next'
 import { useAppContext } from 'context/state'
 import classNames from 'classnames'
 import GameIFrameHeader from './GameIFrameHeader'
+import IframeResizer from 'iframe-resizer-react'
+
 interface Item extends IGame{
   link?: string
 }
@@ -21,16 +23,11 @@ export default function GameIframeRichy(props: Props) {
     <div className={classNames(styles.root, {[styles.mobile]: appContext.isMobile})}>
      {!appContext.isMobile && <GameIFrameHeader  title={props.game.name} icon={props.game.imageIconSmallUrl ?? props.game.imageIconPreviewUrl}/>}
       <div className={styles.board}>
-        <iframe
-          src={props.session.gameUrl}
-          className={styles.iframe}
-          style={{ width: '100%', minWidth: '100%'}}
-        />
-          {/*props.session && (appContext.isMobile ?
-            <iframe
-           src={props.session.gameUrl}
-           className={styles.iframe}
-           style={{ width: '100%', minWidth: '100%'}}
+        {props.session && (appContext.isMobile ?
+          <iframe
+            src={props.session.gameUrl}
+            className={styles.iframe}
+            style={{ width: '100%', minWidth: '100%'}}
           />: <IframeResizer
             log
             autoResize={!appContext.isMobile}
@@ -38,9 +35,10 @@ export default function GameIframeRichy(props: Props) {
             heightCalculationMethod="bodyScroll"
             src={props.session.gameUrl}
             style={{ width: '1px', minWidth: '100%'}}
-          />)*/}
-          {!props.session && <div className={styles.error}>{t('game_error_unavailable')}</div>}
-        </div>
+          />)}
+        {!props.session && <div className={styles.error}>{t('game_error_unavailable')}</div>}
+
+      </div>
 
     </div>
   )
