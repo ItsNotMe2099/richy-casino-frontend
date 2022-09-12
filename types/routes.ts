a77751e2f65ab369cce4434440d8017047d7aa28
Andrey <a1081515@yandex.ru>
2022-09-12 09:40:22 +0300

    fix aviator

--- types/routes.ts
+++ types/routes.ts
@@ -59,7 +59,7 @@ export class Routes{
     return '/catalog/category/richy'
   }
   static get aviator(){
-    return `/game/${runtimeConfig.GAME_AVIATOR_ID}`
+    return `/game/25313`
   }
   static get poker(){
     return `/game/${runtimeConfig.GAME_POKER_ID}`
