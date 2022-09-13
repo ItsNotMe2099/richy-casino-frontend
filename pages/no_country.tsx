2af4ea657e1dc60752b88bcefc967bf4f1111ead
Andrey <a1081515@yandex.ru>
2022-09-13 16:13:49 +0300

    add no country

---
+++ pages/no_country.tsx
@@ -0,0 +1,5 @@
+import ErrorPage from 'components/for_pages/error/ErrorPage'
+export default function NoCountry() {
+  return <ErrorPage title={'Richy is not available in your country!'} >
+    </ErrorPage>
+}
